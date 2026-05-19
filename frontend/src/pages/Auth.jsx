e1b06cc6e81dd22d0ccd94dc36344ed4420d0440
huu144 <huu144@gmail.com>
2026-05-19 19:46:17 +0700

    Update Google and Facebook social login icons and text

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -101,12 +101,12 @@ export const Login = () => {
 
     // Tải Facebook SDK
     if (!document.getElementById('facebook-sdk-script')) {
-      window.fbAsyncInit = function() {
+      window.fbAsyncInit = function () {
         window.FB.init({
-          appId      : FACEBOOK_APP_ID === 'YOUR_FACEBOOK_APP_ID' ? '1234567890' : FACEBOOK_APP_ID,
-          cookie     : true,
-          xfbml      : true,
-          version    : 'v18.0'
+          appId: FACEBOOK_APP_ID === 'YOUR_FACEBOOK_APP_ID' ? '1234567890' : FACEBOOK_APP_ID,
+          cookie: true,
+          xfbml: true,
+          version: 'v18.0'
         });
       };
       const fbScript = document.createElement('script');
@@ -163,7 +163,7 @@ export const Login = () => {
             // Lấy thông tin tài khoản Google thật từ Google API
             const userInfoResponse = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${tokenResponse.access_token}`);
             const googleUser = await userInfoResponse.json();
-            
+
             // Đồng bộ tài khoản Google thật này xuống database PostgreSQL!
             const result = await loginWithSocial({
               provider: 'Google',
@@ -216,7 +216,7 @@ export const Login = () => {
         window.FB.api('/me', { fields: 'name, email, picture.type(large)' }, async (fbUser) => {
           if (fbUser && fbUser.email) {
             const avatarUrl = fbUser.picture && fbUser.picture.data ? fbUser.picture.data.url : '';
-            
+
             // Đồng bộ tài khoản Facebook thật này xuống database PostgreSQL!
             const result = await loginWithSocial({
               provider: 'Facebook',
@@ -318,10 +318,17 @@ export const Login = () => {
         {/* Social Buttons */}
         <div className="auth-socials">
           <button type="button" className="auth-social-btn" onClick={handleGoogleLoginReal}>
-            <span style={{ fontSize: 18 }}>🇬</span> Google thật
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
+              <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" />
+              <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" />
+              <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335" />
+            </svg> Google
           </button>
           <button type="button" className="auth-social-btn" onClick={handleFacebookLoginReal}>
-            <span style={{ fontSize: 18 }}>📘</span> FB thật
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.469h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.469h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" fill="#1877F2" />
+            </svg> FB
           </button>
         </div>
 
@@ -376,12 +383,12 @@ export const Register = () => {
       document.head.appendChild(gScript);
     }
     if (!document.getElementById('facebook-sdk-script')) {
-      window.fbAsyncInit = function() {
+      window.fbAsyncInit = function () {
         window.FB.init({
-          appId      : FACEBOOK_APP_ID === 'YOUR_FACEBOOK_APP_ID' ? '1234567890' : FACEBOOK_APP_ID,
-          cookie     : true,
-          xfbml      : true,
-          version    : 'v18.0'
+          appId: FACEBOOK_APP_ID === 'YOUR_FACEBOOK_APP_ID' ? '1234567890' : FACEBOOK_APP_ID,
+          cookie: true,
+          xfbml: true,
+          version: 'v18.0'
         });
       };
       const fbScript = document.createElement('script');
@@ -463,7 +470,7 @@ export const Register = () => {
           if (tokenResponse && tokenResponse.access_token) {
             const userInfoResponse = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${tokenResponse.access_token}`);
             const googleUser = await userInfoResponse.json();
-            
+
             const result = await loginWithSocial({
               provider: 'Google',
               fullName: googleUser.name,
@@ -513,7 +520,7 @@ export const Register = () => {
         window.FB.api('/me', { fields: 'name, email, picture.type(large)' }, async (fbUser) => {
           if (fbUser && fbUser.email) {
             const avatarUrl = fbUser.picture && fbUser.picture.data ? fbUser.picture.data.url : '';
-            
+
             const result = await loginWithSocial({
               provider: 'Facebook',
               fullName: fbUser.name,
@@ -603,7 +610,7 @@ export const Register = () => {
             {form.password && (
               <div className="auth-strength">
                 <div className="auth-strength-bars">
-                  {[1,2,3,4].map(i => (
+                  {[1, 2, 3, 4].map(i => (
                     <div key={i} className="auth-strength-bar"
                       style={{ background: i <= strength ? strengthColor : 'rgba(255,255,255,0.1)' }} />
                   ))}
@@ -653,10 +660,17 @@ export const Register = () => {
 
         <div className="auth-socials">
           <button type="button" className="auth-social-btn" onClick={handleGoogleLoginReal}>
-            <span style={{ fontSize: 18 }}>🇬</span> Google thật
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4" />
+              <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853" />
+              <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" />
+              <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335" />
+            </svg> Google
           </button>
           <button type="button" className="auth-social-btn" onClick={handleFacebookLoginReal}>
-            <span style={{ fontSize: 18 }}>📘</span> FB thật
+            <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.469h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.469h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" fill="#1877F2" />
+            </svg> Facebook
           </button>
         </div>
 
